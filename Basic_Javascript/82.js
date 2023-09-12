6716c660f77263de025e80f6b701d8aea28dc962
Sgtein <acrestein.bagz@gmail.com>
2023-09-12 10:26:59 +0800

    82

---
+++ Basic_Javascript/82.js
@@ -0,0 +1,10 @@
+// Build JavaScript Objects
+const myDog = {
+    // Only change code below this line
+    "name": "Blue",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Luffy", "Eren", "Bambam"]
+  
+    // Only change code above this line
+  };
