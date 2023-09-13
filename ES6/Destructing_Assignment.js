f973bb6f39fc03b65f8fd7a26fb1d26bf8c1ba2a
Sgtein <acrestein.bagz@gmail.com>
2023-09-13 11:04:47 +0800

    desass

--- ES6/Destructing_Assignment.js
+++ ES6/Destructing_Assignment.js
@@ -1,1 +1,13 @@
 // Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  const { today, tomorrow } = HIGH_TEMPERATURES;
+  // const today = HIGH_TEMPERATURES.today;
+  // const tomorrow = HIGH_TEMPERATURES.tomorrow;
+  
+  // Only change code above this line
